6c34f8e0cdcef88b2cce9f1d681bfdb6ca25992a
Amy Reed <amy_hindman@yahoo.com>
2014-09-08 05:22:29 -0500

    Removed unneeded test files from repo

--- connexus/importjson.py
+++
@@ -1,7 +0,0 @@
-import json
-
-jsoninput = {'userid':1234,'streamname':'amystream','subscriberlist':['subscriber1','subscriber2'],'taglist':['tag1','tag2'],'url':'amy@yahoo.com'}
-myfile = open('currentjson.txt','w')
-data = json.dumps(jsoninput)
-myfile.write(str(data))
-myfile.close()
