92943c4196a14b02ed832a04eb31cee1e86739ab
Amy Reed <amy_hindman@yahoo.com>
2014-09-09 05:53:02 -0500

    added comment for purpose of createimagejson.py

--- connexus/createimagejson.py
+++ connexus/createimagejson.py
@@ -1,7 +1,9 @@
 import json
 import base64
 
-
+#Encodes an image file into base64 and then sends to json object
+#dumps the json object to a text file so it can be used in
+#postman w/upload image
 with open("amypic.jpg", "rb") as f:
     data = f.read()
     picdata = data.encode("base64")
